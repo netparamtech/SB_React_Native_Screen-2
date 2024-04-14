bca621e7b12d386eeea3cf9cd0ed3b45e3ec8cbb
jitend59 <netparam.jitendra59@gmail.com>
2024-04-15 02:03:46 +0530

    Commiting local changes before revert

--- src/Screens/Business.js
+++ src/Screens/Business.js
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
-    backgroundColor:'#1111'
+    backgroundColor:'#1272'
   },
   businessCard: {
     backgroundColor: '#fff',
